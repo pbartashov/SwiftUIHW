cdfbf6e5eb765a51661c7b4625c7318fcc3c3ada
pbartashov <p.bartashov@icloud.com>
2022-10-25 08:52:56 +1000

    Initial Commit

---
+++ SwiftUIHW/SwiftUIHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHWApp.swift
+//  SwiftUIHW
+//
+//  Created by Павел Барташов on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
